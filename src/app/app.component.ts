3e55cd714225cb1137427fad1ccd2997a915becb
Magnus Sundström <kmvsundstrom@gmail.com>
2019-06-12 14:55:04 +0200

    Repointed landing page to home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './home/home.component.html',
+  styleUrls: ['./home/home.component.css']
 })
 export class AppComponent {
-  title = 'PokeApp';
+  title = 'PokéApp';
 }
