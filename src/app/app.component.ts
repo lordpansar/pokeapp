b5f949cfc029096a79746a1d9d802dc4406d91dd
Magnus Sundström <kmvsundstrom@gmail.com>
2019-06-14 10:19:40 +0200

    Repointed app-root selector to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './home/home.component.html',
-  styleUrls: ['./home/home.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'PokéApp';
